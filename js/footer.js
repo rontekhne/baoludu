cfc611140365419ee29659636b10fb384c9682da
ron <rontekhne@users.github.noreply.com>
2023-02-20 18:34:09 -0300

    Refactoring a added post template.

---
+++ js/footer.js
@@ -0,0 +1,14 @@
+function footer()
+{
+    footer = 
+    `
+    <footer>
+        <p>Feito com &hearts; por Ron</p>
+        <p>copyleft <i class="copyleft">&copy</i> 2023</p>
+    </footer>
+    `;
+
+    document.write(footer);
+}
+
+footer();
---
+++ js/post_navbar.js
@@ -0,0 +1,19 @@
+/* when added a new post, update Artigos list */
+
+function navbar()
+{
+    navbar = 
+    `
+    <nav class="navbar" id="mynavbar">
+        <a href="../main.html" class="active"><img src="../img/baoludu-logo.png" alt="Baoludu Logo" height="13"></a>
+        <a href="./post_template.html">Artigos</a>
+        <a href="#">Contato</a>
+        <a href="#">Sobre</a>
+        <a href="javascript:void(0);" class="icon" onclick="doResponsiveNavbar()"><i  id="icon" class="fas fa-hamburger"></i></a>
+    </nav>
+    `;
+
+    document.write(navbar);
+}
+
+navbar();
