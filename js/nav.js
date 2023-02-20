54fe5a8d17698769d89393de007cdfcc018ef57d
ron <rontekhne@users.github.noreply.com>
2023-02-19 21:04:37 -0300

    Navbar has a new hamburger icon with (x) close

--- js/nav.js
+++ js/nav.js
@@ -5,10 +5,16 @@ class to navbar when the user clicks on the icon */
 function doResponsiveNavbar()
 {
     let nav = document.getElementById("mynavbar");
+    let icon = document.getElementById("icon");
 
     if (nav.className === "navbar") {
         nav.className += " responsive";
+        icon.classList.remove("fas", "fa-hamburger");
+        icon.classList.add("fas", "fa-times-circle");
+
     }else {
         nav.className = "navbar";
+        icon.classList.remove("fas", "fa-times-circle");
+        icon.classList.add("fas", "fa-hamburger");
     }
 }
