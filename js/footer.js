a27d4c96c45f89b445f558a63e60dd54d1a619ac
ron <rontekhne@users.github.noreply.com>
2023-02-22 09:27:00 -0300

    added favicon

--- js/footer.js
+++ js/footer.js
@@ -5,6 +5,7 @@ function footer()
     <footer>
         <p>Feito com &hearts; por Ron</p>
         <p>copyleft <i class="copyleft">&copy</i> 2023</p>
+        <a class="footer_links" href="https://github.com/rontekhne" target="_blank"><i class="fa-brands fa-github"></i></a>
     </footer>
     `;
 
