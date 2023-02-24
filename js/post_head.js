7099170149f58fcc626d1a9da41386716a83fee8
Ron <rontekhne@users.github.noreply>
2023-02-24 10:02:01 -0300

    changed meta description location.

--- js/post_head.js
+++ js/post_head.js
@@ -5,6 +5,7 @@ function head()
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Baoludu é um blog voltado ao desenvolvimento de jogos independentes.">
     <link rel="icon" type="image/png" href="../img/favicon.png">
     <link rel="shortcut icon" type="image/png" href="../img/favicon.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
