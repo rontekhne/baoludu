7aa91e0b290077d7cedf466b7c573a23c612bda1
Ron <rontekhne@users.github.noreply>
2023-02-24 09:41:56 -0300

    added shortcut icon for google search results

--- js/post_head.js
+++ js/post_head.js
@@ -6,6 +6,7 @@ function head()
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="../img/favicon.png">
+    <link rel="shortcut icon" type="image/png" href="../img/favicon.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
